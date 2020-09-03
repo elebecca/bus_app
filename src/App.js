c1c85fd22eacd01468d17955faca7dfa33b7cfce
Elena <elena.beccaro@gmail.com>
2020-09-03 15:08:09 +0100

    Add the map in the app

--- src/App.js
+++ src/App.js
@@ -1,9 +1,23 @@
 import React from 'react';
+import { GoogleMap, withScriptjs, withGoogleMap } from "react-google-maps"
 
+function Map() {
+  return <GoogleMap
+    defaultZoom={13}
+    defaultCenter={{ lat: 51.5074, lng: 0.1278 }} />
+}
+const WrappedMap = withScriptjs(withGoogleMap(Map));
 
 function App() {
   return (
-    <div>map here</div>
+    <div style={{ width: '100vw', height: '100vh' }}>
+      <WrappedMap
+        googleMapURL={`https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=geometry,drawing,places`}
+        loadingElement={<div style={{ height: `100%` }} />}
+        containerElement={<div style={{ height: `100%` }} />}
+        mapElement={<div style={{ height: `100%` }} />}
+      />
+    </div >
   );
 }
 
