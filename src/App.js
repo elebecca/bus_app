6b2edebe35e8078962273a9e9612cbd0179251cd
Elena <elena.beccaro@gmail.com>
2020-09-03 17:04:04 +0100

    Adjust defaul coordinate for the city of london

--- src/App.js
+++ src/App.js
@@ -1,16 +1,18 @@
 import React from 'react';
+import './App.css'
 import { GoogleMap, withScriptjs, withGoogleMap } from "react-google-maps"
 
 function Map() {
   return <GoogleMap
     defaultZoom={13}
-    defaultCenter={{ lat: 51.5074, lng: 0.1278 }} />
+    defaultCenter={{ lat: 51.509865, lng: -0.118092 }} />
 }
 const WrappedMap = withScriptjs(withGoogleMap(Map));
 
 function App() {
   return (
-    <div style={{ width: '100vw', height: '100vh' }}>
+    <div classname='container' style={{ width: '70vw', height: '70vh' }}>
+      <h1>BUS APP</h1>
       <WrappedMap
         googleMapURL={`https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=geometry,drawing,places&key=${process.env.REACT_APP_GOOGLE_KEY}`}
         loadingElement={<div style={{ height: `100%` }} />}
